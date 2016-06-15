0d75618e877ac0fe9090c7d855fe6929883bea48
alexandermendes <alexanderhmendes@gmail.com>
2016-06-15 13:59:31 +0100

    Update app config

--- libcrowds_analyst/app.py
+++ libcrowds_analyst/app.py
@@ -1,6 +1,7 @@
 # -*- coding: utf8 -*-
 """Main module for libcrowds-analyst."""
 
+import os
 from libcrowds_analyst import default_settings
 from flask import Flask, request
 from flask_wtf.csrf import CsrfProtect
@@ -12,6 +13,11 @@ def create_app():
     app = Flask(__name__)
     app.config.from_object(default_settings)
     app.config.from_envvar('LIBCROWDS_ANALYST_SETTINGS', silent=True)
+    if not os.environ.get('LIBCROWDS_ANALYST_SETTINGS'):  # pragma: no cover
+        here = os.path.dirname(os.path.abspath(__file__))
+        config_path = os.path.join(os.path.dirname(here), 'settings.py')
+        if os.path.exists(config_path):
+            app.config.from_pyfile(config_path)
     setup_csrf(app)
     setup_url_rules(app)
     setup_auth(app)
