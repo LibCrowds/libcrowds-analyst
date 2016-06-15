79148f340ed7e18316e723a508825f08c83c48bb
alexandermendes <alexanderhmendes@gmail.com>
2016-06-15 13:02:55 +0100

    Remove unused imports

--- run.py
+++ run.py
@@ -1,10 +1,9 @@
 # -*- coding: utf8 -*-
 
-import settings
-from flask import Flask
 from libcrowds_analyst.app import create_app
 
 app = create_app()
+
 
 if __name__ == "__main__":
     app.run(host=app.config['HOST'], port=app.config['PORT'],
