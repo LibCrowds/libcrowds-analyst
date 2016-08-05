5b69c364a63faafea63a2d0832f066aef8a335e9
alexandermendes <alexanderhmendes@gmail.com>
2016-08-05 14:21:21 +0100

    Update tests

--- test/conftest.py
+++ test/conftest.py
@@ -10,6 +10,10 @@ from libcrowds_analyst.core import create_app
 os.environ['LIBCROWDS_ANALYST_SETTINGS'] = '../settings_test.py'
 flask_app = create_app()
 
+task_id = 100
+project_id = 1
+project_shortname = u'short_name'
+
 
 @pytest.fixture(scope='session')
 def app(request):
@@ -35,27 +39,36 @@ def auth_headers():
 
 @pytest.fixture
 def analysis_kwargs():
-    return {'api_key': 'k', 'endpoint': 'e', 'project_short_name':
-            'sn', 'task_id': 123, 'sleep': 0}
+    return {'api_key': 'k', 'endpoint': 'e',
+            'project_short_name': project_shortname, 'task_id': task_id,
+            'sleep': 0}
 
 
 @pytest.fixture
 def result():
-    return enki.pbclient.DomainObject({'id': 1, 'task_id': 1,
+    return enki.pbclient.DomainObject({'id': 1, 'task_id': task_id,
                                        'info': {'n': 1}})
 
 
 @pytest.fixture
 def task():
-    return enki.pbclient.DomainObject({'id': 1})
+    return enki.pbclient.DomainObject({"info": {"key": "value"},
+                                       "n_answers": 30,
+                                       "quorum": 0,
+                                       "calibration": 0,
+                                       "created": "2014-10-22T13:37:58.259609",
+                                       "state": "completed",
+                                       "project_id": project_id,
+                                       "id": task_id,
+                                       "priority_0": 0.0})
 
 
 @pytest.fixture(scope='session')
 def payload():
     pl = dict(fired_at=u'2014-11-17 09:49:27',
-              project_short_name=u'project',
-              project_id=1,
-              task_id=1,
+              project_short_name=project_shortname,
+              project_id=project_id,
+              task_id=task_id,
               result_id=1,
               event=u'task_completed')
     return json.dumps(pl)
--- test/test_analysis.py
+++ test/test_analysis.py
@@ -8,15 +8,12 @@ from libcrowds_analyst import analysis
 
 class TestAnalysis(object):
 
-    def create_tasks(self, n):
-        return [enki.pbclient.DomainObject({'id': i}) for i in range(n)]
-
-    def create_tr_dataframe(self, data):
+    def create_tr_dataframe(self, data, task_id):
         task_runs = []
         for i in range(len(data)):
             tr = {'id': i, 'info': data[i]}
             task_runs.append(enki.pbclient.DomainObject(tr))
-        return {0: enki.dataframer.create_data_frame(task_runs)}
+        return {task_id: enki.dataframer.create_data_frame(task_runs)}
 
     def test_get_valid_analyst_func(self):
         valid_func = analysis.get_analyst_func(1)
@@ -31,38 +28,38 @@ class TestCategoryOneAnalysis(TestAnalysis):
 
     def setup(self):
         super(TestCategoryOneAnalysis, self)
-        self.tr_data = {'oclc': '123', 'shelfmark': '456', 'title': 'card.jpg',
-                        'comments': 'hello'}
-        self.empty_tr_data = {'oclc': '', 'shelfmark': '', 'title': 'card.jpg',
-                              'comments': 'hello'}
+        self.tr_data = {'oclc': '123', 'shelfmark': '456', 'comments': 'hello',
+                        'key': 'value'}
+        self.empty_tr_data = {'oclc': '', 'shelfmark': '', 'comments': 'hello',
+                              'key': 'value'}
 
-    def analyse(self, data, mock_enki, analysis_kwargs):
+    def analyse(self, data, mock_enki, analysis_kwargs, task):
         mock_info = PropertyMock()
         mock_result = MagicMock()
         type(mock_result).info = mock_info
         mock_enki.pbclient.find_results.return_value = [mock_result]
-        mock_enki.Enki().tasks = self.create_tasks(1)
-        mock_enki.Enki().task_runs_df = self.create_tr_dataframe(data)
+        mock_enki.Enki().tasks = [task]
+        mock_enki.Enki().task_runs_df = self.create_tr_dataframe(data, task.id)
         analysis.category_1(**analysis_kwargs)
         return mock_result, mock_info
 
-    def test_cat_1_analysis_with_no_answers(self, mocker, analysis_kwargs):
+    def test_cat_1_analysis_no_answers(self, mocker, analysis_kwargs, task):
         e = mocker.patch('libcrowds_analyst.analysis.enki')
         data = [self.empty_tr_data for i in range(3)]
-        (mock_result, mock_info) = self.analyse(data, e, analysis_kwargs)
+        (mock_result, mock_info) = self.analyse(data, e, analysis_kwargs, task)
         e.pbclient.update_result.assert_called_once_with(mock_result)
         mock_info.assert_called_once_with(self.empty_tr_data)
 
-    def test_cat_1_analysis_with_two_matches(self, mocker, analysis_kwargs):
+    def test_cat_1_analysis_two_matches(self, mocker, analysis_kwargs, task):
         e = mocker.patch('libcrowds_analyst.analysis.enki')
         data = [self.empty_tr_data] + [self.tr_data for i in range(2)]
-        (mock_result, mock_info) = self.analyse(data, e, analysis_kwargs)
+        (mock_result, mock_info) = self.analyse(data, e, analysis_kwargs, task)
         e.pbclient.update_result.assert_called_once_with(mock_result)
         mock_info.assert_called_once_with(self.tr_data)
 
-    def test_cat_1_with_no_matches(self, mocker, analysis_kwargs):
+    def test_cat_1_no_matches(self, mocker, analysis_kwargs, task):
         e = mocker.patch('libcrowds_analyst.analysis.enki')
         data = [self.tr_data] + [self.empty_tr_data for i in range(2)]
-        (mock_result, mock_info) = self.analyse(data, e, analysis_kwargs)
+        (mock_result, mock_info) = self.analyse(data, e, analysis_kwargs, task)
         e.pbclient.update_result.assert_called_once_with(mock_result)
         mock_info.assert_called_once_with('Unanalysed')
