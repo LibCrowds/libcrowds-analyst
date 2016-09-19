e1094a3708425f197bdd79c92c88429daa4920b5
alexandermendes <alexanderhmendes@gmail.com>
2016-09-19 15:32:35 +0100

    Add tests

--- test/test_analysis.py
+++ test/test_analysis.py
@@ -67,3 +67,52 @@ class TestCategoryOneAnalysis(TestAnalysis):
                                                 analysis_kwargs, task)
         mock_client.update_result.assert_called_once_with(mock_result)
         mock_info.assert_called_once_with('Unanalysed')
+
+
+class TestCategorySevenAnalysis(TestAnalysis):
+
+    def setup(self):
+        super(TestCategorySevenAnalysis, self)
+        self.tr_data = {'title': '123', 'author': '456', 'date': 'hello',
+                        'reference': 'xx', 'former_reference': 'yy',
+                        'other_information': 'zz'}
+        self.empty_tr_data = {'title': '', 'author': '', 'date': '',
+                              'reference': '', 'former_reference': '',
+                              'other_information': ''}
+
+    def analyse(self, data, mock_enki, analysis_kwargs, task):
+        mock_info = PropertyMock()
+        mock_result = MagicMock()
+        type(mock_result).info = mock_info
+        mock_enki.pbclient.find_results.return_value = [mock_result]
+        mock_enki.Enki().tasks = [task]
+        mock_enki.Enki().task_runs_df = self.create_tr_dataframe(data, task.id)
+        analysis.category_7(**analysis_kwargs)
+        return mock_result, mock_info
+
+    def test_cat_7_analysis_no_answers(self, mocker, analysis_kwargs, task):
+        mock_enki = mocker.patch('libcrowds_analyst.analysis.enki')
+        mock_client = mocker.patch('libcrowds_analyst.analysis.api_client')
+        data = [self.empty_tr_data for i in range(2)]
+        (mock_result, mock_info) = self.analyse(data, mock_enki,
+                                                analysis_kwargs, task)
+        mock_client.update_result.assert_called_once_with(mock_result)
+        mock_info.assert_called_once_with(self.empty_tr_data)
+
+    def test_cat_7_analysis_two_matches(self, mocker, analysis_kwargs, task):
+        mock_enki = mocker.patch('libcrowds_analyst.analysis.enki')
+        mock_client = mocker.patch('libcrowds_analyst.analysis.api_client')
+        data = [self.tr_data for i in range(2)]
+        (mock_result, mock_info) = self.analyse(data, mock_enki,
+                                                analysis_kwargs, task)
+        mock_client.update_result.assert_called_once_with(mock_result)
+        mock_info.assert_called_once_with(self.tr_data)
+
+    def test_cat_7_no_matches(self, mocker, analysis_kwargs, task):
+        mock_enki = mocker.patch('libcrowds_analyst.analysis.enki')
+        mock_client = mocker.patch('libcrowds_analyst.analysis.api_client')
+        data = [self.tr_data] + [self.empty_tr_data]
+        (mock_result, mock_info) = self.analyse(data, mock_enki,
+                                                analysis_kwargs, task)
+        mock_client.update_result.assert_called_once_with(mock_result)
+        mock_info.assert_called_once_with('Unanalysed')
