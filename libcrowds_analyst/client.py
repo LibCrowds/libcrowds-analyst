80f5ac48ce7ed24c4568e497a0259573b38d07bf
alexandermendes <alexanderhmendes@gmail.com>
2016-11-13 15:59:53 +0000

    Refactor client

--- libcrowds_analyst/client.py
+++ libcrowds_analyst/client.py
@@ -2,7 +2,6 @@
 """API client module for libcrowds-analyst."""
 
 import enki
-import pbclient
 
 
 class PyBossaClient(object):
@@ -17,34 +16,34 @@ class PyBossaClient(object):
     def init_app(self, app):
         self.api_key = app.config['API_KEY']
         self.endpoint = app.config['ENDPOINT']
-        pbclient.set('api_key', self.api_key)
-        pbclient.set('endpoint', self.endpoint)
+        enki.pbclient.set('api_key', self.api_key)
+        enki.pbclient.set('endpoint', self.endpoint)
 
     def get_results(self, project_id, **kwargs):
         """Return results."""
-        return pbclient.find_results(project_id, all=1, **kwargs)
+        return enki.pbclient.find_results(project_id, all=1, **kwargs)
 
     def get_tasks(self, project_id, **kwargs):
         """Return tasks."""
-        return pbclient.find_tasks(project_id, all=1, **kwargs)
+        return enki.pbclient.find_tasks(project_id, all=1, **kwargs)
 
     def get_task_runs(self, project_id, **kwargs):
         """Return task runs."""
-        return pbclient.find_taskruns(project_id, all=1, **kwargs)
+        return enki.pbclient.find_taskruns(project_id, all=1, **kwargs)
 
     def get_projects(self, **kwargs):
         """Return projects."""
-        return pbclient.find_project(all=1, **kwargs)
+        return enki.pbclient.find_project(all=1, **kwargs)
 
-    def get_task_run_dataframe(project_id, task_id):
+    def update_result(self, result):
+        """Update a result."""
+        return enki.pbclient.update_result(result)
+
+    def get_task_run_dataframe(self, project_id, task_id):
         """Return a dataframe containing all task run info for a task."""
-        p = self.find_projects(id=project_id)[0]
+        p = self.get_projects(id=project_id)[0]
         e = enki.Enki(self.api_key, self.endpoint, p.short_name, all=1)
         e.get_tasks(task_id=task_id)
         e.get_task_runs()
         t = e.tasks[0]
         return e.task_runs_df[t.id]
-
-    def update_result(self, result):
-        """Update a result."""
-        return pbclient.update_result(result)
---
+++ test/test_client.py
@@ -0,0 +1,24 @@
+# -*- coding: utf8 -*-
+
+import enki
+from pytest_mock import mocker
+from libcrowds_analyst.client import PyBossaClient
+
+
+class TestClient(object):
+    """Test client module."""
+
+    def test_imported_task_run_df(self, task, project, mocker, app):
+        """Test task run dataframe is created using the correct data."""
+        mock_client = mocker.patch('libcrowds_analyst.client.enki.pbclient')
+        mock_enki = mocker.patch('libcrowds_analyst.client.enki.Enki')
+        mock_enki = enki.Enki(endpoint='endpoint', api_key='api_key',
+                              project_short_name=project.short_name)
+        client = PyBossaClient(app)
+        expected_df = mock_enki.task_runs_df[task.id]
+        df = client.get_task_run_dataframe(project.id, task.id)
+
+        mock_client.find_project.assert_called_with(all=1, id=project.id)
+        mock_enki.get_tasks.assert_called_with(task_id=task.id)
+        mock_enki.get_task_runs.assert_called
+        assert df == expected_df
