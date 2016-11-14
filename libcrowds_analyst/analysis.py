33e5629021a50144f215024e00f2d657cd4c5a43
alexandermendes <alexanderhmendes@gmail.com>
2016-11-14 01:48:16 +0000

    Update analyst

--- libcrowds_analyst/analysis.py
+++ libcrowds_analyst/analysis.py
@@ -4,8 +4,8 @@
 import sys
 import time
 import enki
-import pbclient
 import numpy as np
+from libcrowds_analyst.core import pybossa_client
 
 
 #def _concat(df, col):
@@ -33,16 +33,6 @@ import numpy as np
 #    df[col].replace(r'(?i)^chi', 'CHI', inplace=True)
 
 
-def _get_task_run_dataframe(short_name, task_id):
-    """Return a dataframe containing all task run info for a task."""
-    p = pbclient.get_project(project_id)
-    e = enki.Enki(pbclient.api_key, pbclient.endpoint, p.short_name, all=1)
-    e.get_tasks(task_id=task_id)
-    e.get_task_runs()
-    t = e.tasks[0]
-    return e.task_runs_df[t.id]
-
-
 def _extract_keys(df):
     """Return all keys from all task run info fields."""
     keyset = set()
@@ -59,38 +49,37 @@ def _drop_empty_rows(df):
     return df
 
 
-def analyse(project_id, task_id, match_percentage=60):
+def analyse(project_id, task_id, match_percentage):
     """Analyser for all LibCrowds projects.
 
-    Check that the info fields of each task run match n percent of the time or
-    above, disregarding task runs where all of the info fields are blank. For
-    tasks where we have a sufficent number of matches the result will be set to
-    a dictionary containing the task run info keys and the matched values. For
-    tasks where all info fields of all task runs are empty the result will be
-    set to a dictionary containing the task run info keys and empty values. For
-    all other tasks the result will be set to the string 'Unanalysed' so that
-    the different answers can be checked manually later.
+    Check that the info fields of each task run match n percent of the time
+    or above, disregarding task runs where all of the info fields are blank.
+    For tasks where we have a sufficent number of matches the result will be
+    set to a dictionary containing the task run info keys and the matched
+    values. For tasks where all info fields of all task runs are empty the
+    result will be set to a dictionary containing the task run info keys and
+    empty values. For all other tasks the result will be set to the string
+    'Unanalysed' so that the different answers can be checked manually later.
     """
     time.sleep(2)  # To handle API rate limit when analysing many results
-    r = pbclient.find_results(project_id, task_id=task_id, limit=1, all=1)[0]
-    df = _get_task_run_dataframe(project_id, task_id)
+    df = pybossa_client.get_task_run_dataframe(project_id, task_id)
     keys = _extract_keys(df)
     df = df[keys]
     df = _drop_empty_rows(df)
+    r = pybossa_client.get_results(project_id, task_id=task_id, limit=1)[0]
 
     if df.empty:
         r.info = {k: "" for k in keys}
-        pbclient.update_result(r)
+        pybossa_client.update_result(r)
         return
 
     # Check for n percent of matches
-    df = df[df.duplicated(keys, keep=False)]
-    if not df.dropna(how='all').empty:
-        r.info = dict(oclc=df.iloc[0]['oclc'],
-                      shelfmark=df.iloc[0]['shelfmark'], comments=comments)
-        pbclient.update_result(r)
-        return
-
-    # Unanalysed result
-    r.info = 'Unanalysed'
-    pbclient.update_result(r)
+    required_matches = int(round(len(df.index)*(match_percentage / 100.0)))
+    info = {}
+    for k in keys:
+        if df[k].value_counts().max() < required_matches:
+            info = 'Unanalysed'
+            continue
+        info[k] = df[k].value_counts().idxmax()
+    r.info = info
+    pybossa_client.update_result(r)
