fe4bccbf399c5e7d652753d32470f1e9f53ba287
alexandermendes <alexanderhmendes@gmail.com>
2016-12-31 00:40:22 +0000

    Refactor analysis view

--- pybossa_analyst/view/analysis.py
+++ pybossa_analyst/view/analysis.py
@@ -2,12 +2,12 @@
 """View module for pybossa-analyst."""
 
 import os
-import time
+import pbclient
 from redis import Redis
 from rq import Queue
 from flask import Blueprint
 from flask import render_template, request, abort, flash, redirect, url_for
-from flask import current_app, send_file, Response
+from flask import current_app, send_file, Response, session
 from werkzeug.utils import secure_filename
 from pybossa_analyst import analysis, forms, zip_builder, client
 from pybossa_analyst.login import login_required
@@ -23,26 +23,34 @@ MINUTE = 60
 @login_required
 def index():
     """Projects view."""
-    return "projects"
+    pbclient.set('api_key', session['api_key'])
+    pbclient.set('endpoint', current_app.config['ENDPOINT'])
+    projects = pbclient.find_project()
+    if not projects:
+        abort(404)
+    return render_template('index.html', projects=projects,
+                           api_key=session['api_key'])
 
 
 @blueprint.route('/<short_name>/')
 @login_required
-def analyse_next_empty_result(short_name):
+def analyse(short_name):
     """View for analysing the next empty result."""
-    api_key = current_app.config['API_KEY']
-    endpoint = current_app.config['ENDPOINT']
-    try:
-        p = client.get_projects(api_key, endpoint, short_name=short_name,
-                                limit=1)[0]
-    except IndexError:  # pragma: no cover
-        abort(404)
+    pbclient.set('api_key', session['api_key'])
+    pbclient.set('endpoint', current_app.config['ENDPOINT'])
+    projects = pbclient.find_project(short_name=short_name, limit=1)
+    if not projects:
+        if pbclient.find_project(short_name=short_name, limit=1, all=1):
+            abort(403)
+        else:
+            abort(404)
 
-    results = client.get_results(api_key, endpoint, p.id, limit=1,
-                                 info='Unanalysed')
+    project = projects[0]
+    results = pbclient.find_results(project.id, limit=1, info='Unanalysed')
     if not results:  # pragma: no cover
         flash('There are no unanlysed results to process!', 'success')
         return redirect(url_for('.index'))
+
     result = results[0]
     return redirect(url_for('.analyse_result', short_name=short_name,
                             result_id=result.id))
@@ -52,35 +60,34 @@ def analyse_next_empty_result(short_name):
 @login_required
 def analyse_result(short_name, result_id):
     """View for analysing a result."""
-    api_key = current_app.config['API_KEY']
-    endpoint = current_app.config['ENDPOINT']
-    try:
-        project = client.get_projects(api_key, endpoint,
-                                      short_name=short_name)[0]
-    except IndexError:  # pragma: no cover
+    pbclient.set('api_key', session['api_key'])
+    pbclient.set('endpoint', current_app.config['ENDPOINT'])
+    projects = pbclient.find_project(short_name=short_name, limit=1)
+    if not projects:
+        if pbclient.find_project(short_name=short_name, limit=1, all=1):
+            abort(403)
+        else:
+            abort(404)
+
+    project = projects[0]
+    results = pbclient.find_results(project.id, limit=1)
+    if not results:  # pragma: no cover
         abort(404)
 
-    try:
-        result = client.get_results(api_key, endpoint, project.id,
-                                    id=result_id)[0]
-    except (IndexError, ValueError):  # pragma: no cover
-        abort(404)
-
+    result = results[0]
     if request.method == 'POST':
         data = request.form.to_dict()
         data.pop('csrf_token', None)
         result.info = data
-        client.update_result(api_key, endpoint, result)
-        url = url_for('.analyse_next_empty_result', short_name=short_name)
+        pbclient.update_result(result)
+        url = url_for('.analyse', short_name=short_name)
         return redirect(url)
 
-    task = client.get_tasks(api_key, endpoint, project.id,
-                            id=result.task_id)[0]
-    taskruns = client.get_task_runs(api_key, endpoint, project.id,
-                                    task_id=task.id)
-    exclude = current_app.config['EXCLUDED_KEYS']
+    task = pbclient.find_tasks(project.id, id=result.task_id)[0]
+    taskruns = pbclient.find_taskruns(project.id, task_id=task.id)
+    excluded_keys = current_app.config['EXCLUDED_KEYS']
     keys = set(k for tr in taskruns for k in tr.info.keys()
-               if k not in exclude)
+               if k not in excluded_keys)
     return render_template('analyse.html', project=project, result=result,
                            task=task, task_runs=taskruns,
                            title=project.name, keys=keys)
@@ -90,14 +97,16 @@ def analyse_result(short_name, result_id):
 @login_required
 def reanalyse(short_name):
     """View for triggering reanalysis of all results."""
-    api_key = current_app.config['API_KEY']
-    endpoint = current_app.config['ENDPOINT']
-    try:
-        project = client.get_projects(api_key, endpoint,
-                                      short_name=short_name)[0]
-    except IndexError:  # pragma: no cover
-        abort(404)
+    pbclient.set('api_key', session['api_key'])
+    pbclient.set('endpoint', current_app.config['ENDPOINT'])
+    projects = pbclient.find_project(short_name=short_name, limit=1)
+    if not projects:
+        if pbclient.find_project(short_name=short_name, limit=1, all=1):
+            abort(403)
+        else:
+            abort(404)
 
+    project = projects[0]
     form = forms.ReanalysisForm(request.form)
     if request.method == 'POST' and form.validate():
         _filter = form.result_filter.data
@@ -124,21 +133,23 @@ def reanalyse(short_name):
 
 @blueprint.route('/<short_name>/download/', methods=['GET', 'POST'])
 @login_required
-def prepare_zip(short_name):
+def download_input(short_name):
     """View to prepare a zip file for download."""
-    api_key = current_app.config['API_KEY']
-    endpoint = current_app.config['ENDPOINT']
-    try:
-        project = client.get_projects(api_key, endpoint,
-                                      short_name=short_name)[0]
-    except IndexError:  # pragma: no cover
-        abort(404)
+    pbclient.set('api_key', session['api_key'])
+    pbclient.set('endpoint', current_app.config['ENDPOINT'])
+    projects = pbclient.find_project(short_name=short_name, limit=1)
+    if not projects:
+        if pbclient.find_project(short_name=short_name, limit=1, all=1):
+            abort(403)
+        else:
+            abort(404)
 
+    project = projects[0]
     form = forms.DownloadForm(request.form)
     if request.method == 'POST' and form.validate():
         importer = form.importer.data
         task_ids = form.task_ids.data.split()
-        tasks = client.get_tasks(api_key, endpoint, project_id=project.id)
+        tasks = pbclient.find_tasks(project_id=project.id)
         valid_task_ids = [str(t.id) for t in tasks]
         tasks_to_export = [t for t in tasks if str(t.id) in task_ids and
                            str(t.id) in valid_task_ids]
@@ -147,8 +158,8 @@ def prepare_zip(short_name):
         if invalid_ids:
             msg = 'The following task IDs are invalid: {0}'.format(invalid_ids)
             flash(msg, 'danger')
-            return render_template('prepare_zip.html', project=project,
-                                   title="Download task input", form=form)
+            return render_template('download_input.html', project=project,
+                                   title="Download Input", form=form)
 
         ts = int(time.time())
         fn = '{0}_task_input_{1}.zip'.format(short_name, ts)
@@ -162,5 +173,5 @@ def prepare_zip(short_name):
     elif request.method == 'POST':  # pragma: no cover
         flash('Please correct the errors.', 'danger')
 
-    return render_template('prepare_zip.html', title="Download task input",
+    return render_template('download_input.html', title="Download Input",
                            project=project, form=form)
