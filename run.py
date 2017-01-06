31c152cae512caa6620d8e3e35dd9de3ba0db541
alexandermendes <alexanderhmendes@gmail.com>
2017-01-06 16:00:33 +0000

    Run app threaded in development

--- run.py
+++ run.py
@@ -6,6 +6,6 @@ from pybossa_analyst.core import create_app
 if __name__ == "__main__":
     app = create_app()
     app.run(host=app.config['HOST'], port=app.config['PORT'],
-            debug=app.config['DEBUG'])
+            debug=app.config['DEBUG'], threaded=app.config['DEBUG'])
 else:
     app = create_app()
