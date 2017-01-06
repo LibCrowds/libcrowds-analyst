2e528a35405b78f36547389db2d2bd51ba0b8403
alexandermendes <alexanderhmendes@gmail.com>
2017-01-06 17:40:15 +0000

    Add all=1 to auth check

--- pybossa_analyst/view/projects.py
+++ pybossa_analyst/view/projects.py
@@ -26,8 +26,8 @@ def _configure_pbclient():
 
 def _ensure_authorized_to_update(short_name):
     """Ensure that a result can be updated using the current API key."""
-    p = pbclient.find_project(short_name=short_name)[0]
-    r = pbclient.find_results(p.id, limit=1)[0]
+    p = pbclient.find_project(short_name=short_name, all=1)[0]
+    r = pbclient.find_results(p.id, limit=1, all=1)[0]
     resp = pbclient.update_result(r)
     if isinstance(resp, dict) and resp.get('status_code') == 401:
         raise Unauthorized("""You are not authorised to update results for
