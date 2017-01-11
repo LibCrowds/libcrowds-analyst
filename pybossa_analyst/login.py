fead34f265912080ae39f853fc95b185a9aa73ff
alexandermendes <alexanderhmendes@gmail.com>
2017-01-11 12:50:58 +0000

    Pass api key as param

--- pybossa_analyst/login.py
+++ pybossa_analyst/login.py
@@ -7,6 +7,9 @@ from flask import session, request, redirect, url_for
 def login_required(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
+        api_key = request.args.get('api_key')
+        if api_key:
+            session["api_key"] = api_key
         if session.get('api_key') is None:
             return redirect(url_for('home.login', next=request.url))
         return f(*args, **kwargs)
