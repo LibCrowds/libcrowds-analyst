816d66f16024afbf93cafee844d270cf306f8fdb
alexandermendes <alexanderhmendes@gmail.com>
2017-01-12 00:14:39 +0000

    Update tests

---
+++ test/test_auth.py
@@ -0,0 +1,19 @@
+# -*- coding: utf8 -*-
+
+import pytest
+from pytest_mock import mocker
+from werkzeug.exceptions import Unauthorized
+from pybossa_analyst import auth
+
+
+class TestAuth(object):
+
+    def test_unauthorized_to_update(self, mocker, project):
+        """Test unauthorized error raised when error response returned."""
+        mock_pbclient = mocker.patch('pybossa_analyst.auth.pbclient')
+        mock_pbclient.update_result.return_value = {'status_code': 401}
+        with pytest.raises(Unauthorized) as excinfo:
+            auth.ensure_authorized_to_update(project.short_name)
+        err_msg = ("You are not authorised to update results for this project",
+                   "using the API key provided.")
+        assert excinfo.value.description == err_msg
--- test/test_zip_builder.py
+++ test/test_zip_builder.py
@@ -7,16 +7,28 @@ from pybossa_analyst import zip_builder
 
 class TestZipBuilder(object):
 
-    def test_invalid_importer_identified(self, app):
+    def test_invalid_importer_identified(self):
         """Test error raised when attempting to build with invalid importer."""
         with pytest.raises(ValueError) as excinfo:
             zip_builder.generate([], 'unknown_importer')
         assert excinfo.value.message == 'Unknown importer type'
 
-    def test_correct_file_downloaded(self, app, task, mocker):
+    def test_flickr_importer_identified(self, mocker, task):
+        """Test Flickr importer identified."""
+        mock_gen = mocker.patch('pybossa_analyst.zip_builder._generate_zip')
+        zip_builder.generate([task], 'flickr')
+        mock_gen.assert_called_with([task], "title", "url")
+
+    def test_dropbox_importer_identified(self, mocker, task):
+        """Test Dropbox importer identified."""
+        mock_gen = mocker.patch('pybossa_analyst.zip_builder._generate_zip')
+        zip_builder.generate([task], 'dropbox')
+        mock_gen.assert_called_with([task], "filename", "link_raw")
+
+    def test_correct_file_downloaded(self, mocker, task):
         """Test zip file is built with the correct task input."""
-        mock_requests = mocker.patch('pybossa_analyst.zip_builder.requests')
+        mock_download = mocker.patch('pybossa_analyst.zip_builder._download')
         gen = zip_builder._generate_zip([task], 'title', 'url')
         [x for x in gen]
         url = task.info['url']
-        mock_requests.get.assert_called_once_with(url, stream=True)
+        mock_download.assert_called_once_with(url)
