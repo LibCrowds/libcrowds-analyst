bdf2f5f9667c792aa14b0c6e1abe56ead9248d3c
Alex Mendes <alexanderhmendes@gmail.com>
2017-09-11 23:57:02 +0100

    Fix analysis module imports

---
+++ libcrowds_analyst/analysis/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf8 -*-
+"""Analysis module for libcrowds-analyst."""
+
+from libcrowds_analyst.analysis import convert_a_card
+from libcrowds_analyst.analysis import playbills
+
+__all__ = ['convert_a_card', 'playbills']
