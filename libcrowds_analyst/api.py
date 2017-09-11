991080372a91dcc7d321d88c126bfb226244f9d4
Alex Mendes <alexanderhmendes@gmail.com>
2017-09-11 23:39:11 +0100

    Update API

--- libcrowds_analyst/api.py
+++ libcrowds_analyst/api.py
@@ -14,29 +14,16 @@ MINUTE = 60
 
 def analyse(func):
     """Analyse a webhook."""
-    payload = json.loads(request.json) or {}
-    if payload['event'] == 'task_completed':
+    if request.method == 'POST' and payload['event'] != 'task_completed':
+        abort(400)
+    elif request.method == 'POST' and payload['event'] == 'task_completed':
+        payload = json.loads(request.json) or {}
         payload['api_key'] = current_app.config['API_KEY']
         payload['endpoint'] = current_app.config['ENDPOINT']
         payload['doi'] = current_app.config['DOI']
         QUEUE.enqueue_call(func=func, kwargs=payload, timeout=10*MINUTE)
-        response = jsonify({
-            "message": "Accepted",
-            "status": 202
-        })
-        response.status_code = 202
-        return response
-    abort(400)
-
-@BP.route('/', methods=['GET'])
-def index():
-    """Display the endpoints available on the server."""
     response = jsonify({
-        "message": "LibCrowds Analyst",
-        "endpoints": {
-            "/convert-a-card": "For Convert-a-Card webhooks.",
-            "/playbills/select": "For In the Spotlight select task webhooks."
-        },
+        "message": "OK",
         "status": 200
     })
     response.status_code = 200
@@ -45,14 +32,10 @@ def index():
 @BP.route('convert-a-card', methods=['GET', 'POST'])
 def convert_a_card():
     """Endpoint for Convert-a-Card webhooks."""
-    if request.method != 'POST':
-        abort(405)
     return analyse(analysis.convert_a_card.analyse)
 
 
 @BP.route('playbills/select', methods=['GET', 'POST'])
 def playbills_mark():
     """Endpoint for In the Spotlight select task webhooks."""
-    if request.method != 'POST':
-        abort(405)
     return analyse(analysis.playbills.analyse_selections)
