341b3a5a8b0f6494228fc0d7cef23f3307dd4941
Alexander Mendes <alexanderhmendes@gmail.com>
2017-09-13 19:06:50 +0100

    PEP8 fixes

--- test/test_object_loader.py
+++ test/test_object_loader.py
@@ -8,14 +8,16 @@ class TestObjectLoader(object):
 
     def test_correct_objects_are_loaded(self, result):
         """Test that objects are loaded."""
-        def mock_func():
+        def mock_func(**kwargs):
             return [result]
         object_list = object_loader.load(mock_func)
         assert object_list == [result]
 
     def test_all_objects_are_loaded(self, result):
         """Test that all objects are loaded."""
-        def mock_func():
-            return [result]*100
+        long_list = [result]*100
+
+        def mock_func(**kwargs):
+            return long_list
         object_list = object_loader.load(mock_func)
         assert len(object_list) == 200
